db62e6e20aef168c91ac84658af4b07b2f1c86e7
iranmederos@gmail.com <iranmederos@gmail.com>
2022-11-17 17:30:07 -0300

    añadiendo mensajes

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-btn= document.querySelector('.button');
-num= document.querySelector('.number');
-
-btn.addEventListener('click',()=>{
-  n= Math.floor(Math.random()*36)+1;
-  num.innerHTML=`
+btn = document.querySelector('.button');
+num = document.querySelector('.number');
+text = document.querySelector('#text_field');
+btn.addEventListener('click', () => {
+  if (text.value == null || text.value == '') {
+    alert('Debes ingresar una pregunta, te recomiendo pensarla bien');
+    console.log(text.value)
+  } else {
+    alert('Vas a usar el numero que te voy a dar como indice, bucarlo en el Oráculo del guerrero y el te dara tu respuesta, espero te sea de ayuda.')
+    n = Math.floor(Math.random() * 36) + 1;
+    num.innerHTML = `
     <span class='dat'>${n}<span>
   `;
-  
+  }
 });
