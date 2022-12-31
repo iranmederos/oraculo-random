f9730c4c6576fe0a64c781aa5a12c57c6ec65759
Iran Mederos <iranmederos@gmail.com>
2022-12-31 13:55:36 -0300

    actualizanción, ordenamiento y comienzo del libro

---
+++ src/js/book.js
@@ -0,0 +1,35 @@
+const oraculo = [
+    {
+        num: "I",
+
+        titulo: "El Guerrero Nace",
+
+        texto: `La nieve se ha derretido El bosque respira nuevamente. 
+        El Sol se refleja en el estanque... Una vez más.`
+
+    },
+
+    {
+        num: "II",
+
+        tiutlo: "El Guerrero despierta",
+
+        texto: `En el horizonte, el límite entre el Cielo y la Tierra se desdibuja. En el horizonte, es imposible discernir.
+        Antes de mirar la puesta de Sol, me siento en una firme roca.
+        Has estado viviendo en lo irreal. Tu existencia se ha desarrollado sobre lo inexistente. Careces de base. Estás en el aire. Todo este tiempo has vivido pensado que las cosas eran de una manera. Has actuado en concordancia a ello y está bien. Pero en el Camino del conocimiento, el Guerrero debe penetrar más y más profundamente en sí mismo y su vida. En el Camino del conocimiento, el Guerrero debe morir para luego renacer fulgurante.
+        En este tiempo de despertar. Bruscamente has tomado conciencia de lo frágiles que eran tus creencias. Lo que antes eran tus pilares son ahora solo vestigios. El altísimo te ha jugado la vieja broma de sacarte la silla cuando ibas a sentarte como todos los días. Te has golpeado, has sentido dolor y esto también está bien. Si hubieses caído sobre algo mullido, no le hubieras dado mayor importancia. Pero la verdad es que aterrizaste a otra realidad. Estás desconcertado. Tu conciencia obnubilada. No entiendes nada de lo que pasó. Sientes que has perdido el tiempo hasta ahora y hasta sentirás vergüenza por lo que antes hiciste. Restrospectivamente, te verás como alguien dedicado a cosas fútiles.
+        El dolor ciega. Aunque no lo entiendas ahora, lo que te ha ocurrido es algo maravilloso. Ya no eres el mismo de hace unos momentos atrás y, si eres lo suficientemente sabio, nunca volverás a serlo. Otras cosas importarán ahora, otras montañas habras de subir. Otros Demonios enfrentarás, otros hermanos caminarán contigo. Vive tu nueva vida, mira con tus nuevos ojos. Sin embargo, prepárate a renacer. Nuevamente. Sólo así podrás llegar al final del camino.`
+
+    },
+
+    {
+        num: "III",
+
+        tiutlo: "El Guerrero emprende el viaje",
+
+        texto: `Has estado demasiado tiempo en este lugar. El aire está enrarecido. Estás estancado, no fluyes. Tu entorno ya no te aporta nada nuevo. Debes, pues, marcharte en busca de nuevos amaneceres. Deja lo superfluo, lleva contigo sólo lo esencial. Tu corazón, tu espíritu y tu cuerpo son suficientes.
+        Ordena las cosas antes de irte. Cierra los círculos. No dejes cabos sueltos. Que tu partida sea natural, como el migrar de las grullas en invierno. No escapes. Simplemente abandona este lugar, esta situación. Vete. De seguro encontrarás nuevos compañeros de viaje.
+        Tal vez algún día regreses.`
+    },
+
+]
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  btn = document.querySelector('.button');
+  num = document.querySelector('.number');
+  index = 0;
+  btn.addEventListener('click', () => {
+    text = document.querySelector('#text_field').value;
+    if (text == null || text == '' || text.length < 5) {
+      alert('Debes ingresar una pregunta, te recomiendo pensarla bien');
+      console.log(text.value)
+    } else {
+      n = Math.floor(Math.random() * 36) + 1;
+      index = n;
+      num.innerHTML = `
+    <span class='dat'>${n}<span>
+  `;
+    }
+  });
+  console.log(oraculo)
+
+}
