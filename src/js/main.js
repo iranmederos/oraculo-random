f8bd25a298855e75cc633b269a1fad400aec4fab
Iran Mederos <iranmederos@gmail.com>
2024-05-21 15:50:45 -0300

    minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ window.onload = () => {
     btn = document.querySelector('.button');
     btn.addEventListener('click', () => {
         text = document.querySelector('#text_field').value;
-        if (text == null || text == '' || text.length < 5) {
+        if (text == null || text === '' || text.length < 5) {
             const errorMessage = 'Debes ingresar una pregunta de al menos 5 caracteres.';
             alert(errorMessage);
             text.focus();
